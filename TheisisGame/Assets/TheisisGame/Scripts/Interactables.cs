866022ccac42899c573cbfaed04acf007f1eb9fa
karmasquid <felare9001@hotmail.com>
2018-12-10 14:51:31 +0100

    minor stuff
    Inte mycket att bry sig om.

---
+++ TheisisGame/Assets/TheisisGame/Scripts/Interactables.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Interactables : MonoBehaviour {
+    public float radius = 2f;
+    GameObject player;
+    /*void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);   
+    } */
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
