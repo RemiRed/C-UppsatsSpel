94a7bc2fdeae4b956227c7479edda903b7bf49c7
karmasquid <felare9001@hotmail.com>
2018-12-12 15:24:04 +0100

    går att läsa skyltar
    E på tangentbordet och Y på kontrollen. Har lagt till en collider som går igång när man ställer sig i den. Vet inte hur jag ska ändra just den men triggern måste ändras, tänker att det borde gå i ett och samma script.

---
+++ TheisisGame/Assets/TheisisGame/Scripts/IsInteractable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IsInteractable : MonoBehaviour {
+    [SerializeField]
+    private Image signText;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyUp(KeyCode.E) || Input.GetButtonUp("Y"))
+            signText.enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            signText.enabled = false;
+    }
+}
---
+++ TheisisGame/Assets/TheisisGame/Scripts/TutorialTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialTrigger : MonoBehaviour {
+    [SerializeField]
+    private Image popUpText;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            popUpText.enabled = true;
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            popUpText.enabled = false;
+
+    }
+
+}
