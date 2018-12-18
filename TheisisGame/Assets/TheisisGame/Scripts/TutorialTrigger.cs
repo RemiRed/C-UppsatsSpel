5b2186669080ea2afeeca71a6eab2b64627086c1
karmasquid <felare9001@hotmail.com>
2018-12-18 08:28:17 +0100

    Lite pynt och script
    Förberedde banan för screenshots och så att det inte gör ont i ögonen av att titta på allt vitt som reflekteras på det andra vita. Har förberett lite för att triggerscriptet endast ska gå igång en gång.

--- TheisisGame/Assets/TheisisGame/Scripts/TutorialTrigger.cs
+++ TheisisGame/Assets/TheisisGame/Scripts/TutorialTrigger.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class TutorialTrigger : MonoBehaviour {
+    bool pauseState = false;
     [SerializeField]
     private Image popUpText;
+
+
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             popUpText.enabled = true;
+            pauseState = true;
+            print(pauseState);
+            //Spelet pausas här.
+        }
 
+        else if (Input.GetKeyUp(KeyCode.E) && pauseState == true || Input.GetButtonUp("Y") && pauseState == true)
+        {
+            popUpText.enabled = false;
+            print(pauseState);
+            //Spelet fortsätter här.
+        }
     }
     void OnTriggerExit(Collider other)
     {
